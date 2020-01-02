4479b065184864a5045da70089101930631c22dd
Steve Anderson <steve@bootladder.com>
2020-01-01 23:38:24 -0500

    day1 part2

--- day1.hs
+++ day1.hs
@@ -2,12 +2,24 @@
 main = do
   s <- readFile "day1-input.txt"
   let nonEmptyLines = filter (\x -> length x > 0) (lines s)
-  let masses = map read $ nonEmptyLines :: [Float]
+  let masses = map read $ nonEmptyLines :: [Int]
   mapM_ (putStrLn . show) masses
 
   let requiredFuels = map getRequiredFuel masses
   putStrLn $ show $ sum requiredFuels
 
+  -- part 2
+  let requiredFuels = map getRequiredFuelIncludingFuel masses
+  putStrLn $ show $ sum requiredFuels
 
-getRequiredFuel :: Float -> Int
-getRequiredFuel x = (floor (x/3)) - 2
+
+getRequiredFuel :: Int -> Int
+getRequiredFuel x = (floor ((fromIntegral x)/3)) - 2
+
+getRequiredFuelIncludingFuel  :: Int -> Int
+getRequiredFuelIncludingFuel x =
+  let requiredFuel = getRequiredFuel x
+  in
+    if requiredFuel > 0
+    then requiredFuel + (getRequiredFuelIncludingFuel requiredFuel)
+    else 0
