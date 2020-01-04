bc468d058902dcf874b9b00cd6fd7864f96c15ca
Steve Anderson <steve@bootladder.com>
2020-01-04 01:10:54 -0500

    day 4 done

--- day4.hs
+++ day4.hs
@@ -10,8 +10,16 @@ main = do
   let possibilities = filter isPossiblePassword range
   putStrLn $ show $ length possibilities
 
+  putStrLn "Part 2"
+  let possibilities = filter isPossiblePasswordPart2 range
+  putStrLn $ show $ length possibilities
+
 isPossiblePassword i =
   (hasTwoAdjacentDigits i) &&
+  (digitsNeverDecrease i) 
+
+isPossiblePasswordPart2 i = 
+  (hasExactlyTwoAdjacentDigits i) &&
   (digitsNeverDecrease i) 
 
 
@@ -33,3 +41,16 @@ digitsNeverDecrease i = digitsNeverDecrease' $ digitsOfInt i
       if (head xs) < x
       then False
       else digitsNeverDecrease' xs
+
+
+hasExactlyTwoAdjacentDigits i =
+  let paddedDigits = [-1] ++ (digitsOfInt i) ++ [-1]
+  in
+    listHasExactlyTwoAdjacentMembers paddedDigits
+
+listHasExactlyTwoAdjacentMembers (x1:x2:x3:[]) = False
+listHasExactlyTwoAdjacentMembers (x1:x2:[]) = False
+listHasExactlyTwoAdjacentMembers (x1:x2:x3:xs) =
+  if x1 /= x2 && x2 == x3 && x3 /= (head xs)
+  then True
+  else listHasExactlyTwoAdjacentMembers (x2:x3:xs)
