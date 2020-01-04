bb901ef136d087d9a5252f1d2c1fb481f101f8fe
Steve Anderson <steve@bootladder.com>
2020-01-04 17:43:00 -0500

    refactor original computer to use data and record types

--- day5.hs
+++ day5.hs
@@ -1,51 +1,66 @@
-
-main = do
-  s <- readFile "day5-input.txt"
-  putStrLn "Running Short Test Programs"
-  let testProgram1 = [1002,4,3,4,33]
-  let testOut1 = runProgram testProgram1
-  putStrLn $ show testOut1
-  putStrLn "hello"
-
 type Program = [Int]
 type Counter = Int 
-type Status  = Int
+data Status  = OK|HALTED|BADINSTRUCTION deriving Show
 data ComputerState =
   ComputerState { program :: Program 
                 , counter :: Counter 
                 , status :: Status 
                 }deriving Show
 
+testProgram1 = [2,4,5,4,33,3]
+testProgram2 = [1002,4,3,4,33]
+
+main = do
+  s <- readFile "day5-input.txt"
+  putStrLn "Running Short Test Programs"
+  
+  let testOut1 = runProgram testProgram1
+  putStrLn $ show testOut1
+  putStrLn "hello"
+
+
 runProgram :: Program -> ComputerState
 runProgram p =
-  let cs = ComputerState p 0 0
+  let cs = ComputerState p 0 OK
   in
     runProgram' cs
 
 runProgram' :: ComputerState -> ComputerState
 runProgram' cs =
-  if (program cs !! counter cs) == 99
-  then
-    ComputerState (program cs) (counter cs) 888
-  else
-    runProgram' $ ComputerState (executeAtPosition (counter cs) (program cs)) (counter cs +4) 999
+  case (status cs) of
+    OK ->
+                       
+      if (program cs !! counter cs) == 99 --halt
+      then
+        cs {status = HALTED}
+      else
+        let afterExecute = executeAtPosition cs
+            afterUpdate = updateCounter afterExecute
+        in
+            runProgram' afterUpdate
 
+    _ -> cs
+  
+updateCounter :: ComputerState -> ComputerState
+updateCounter cs = cs {counter = (counter cs) + 4}
 
-executeAtPosition :: Int -> [Int] -> [Int]
-executeAtPosition pos program =
-  let instruction = getIntcodeAt pos program
+executeAtPosition :: ComputerState -> ComputerState
+executeAtPosition cs =
+  let instruction = getIntcodeAt (counter cs) (program cs)
       intcode = head instruction
-  in case intcode of
+  in
+    case intcode of
     1 ->
-      let newProgram = add (instruction !! 1) (instruction !! 2) (instruction !! 3) program
+      let newProgram = add (instruction !! 1) (instruction !! 2) (instruction !! 3) $ program cs
       in
-        newProgram
+        ComputerState newProgram (counter cs) OK
     2 ->
-      let newProgram = multiply (instruction !! 1) (instruction !! 2) (instruction !! 3) program
+      let newProgram = multiply (instruction !! 1) (instruction !! 2) (instruction !! 3) $ program cs
       in
-        newProgram
+        ComputerState newProgram (counter cs) OK
 
-    _ -> program
+    _ -> ComputerState (program cs) (counter cs) BADINSTRUCTION
+        
     
 
 getIntcodeAt pos program =
