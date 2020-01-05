bb75f1c421afe9b47293b935f67007ef3de7b035
Steve Anderson <steve@bootladder.com>
2020-01-04 20:03:18 -0500

    parse opcode and instruction into types.  support immediate operands

--- day5.hs
+++ day5.hs
@@ -7,14 +7,27 @@ data ComputerState =
                 , status :: Status 
                 }deriving Show
 
-testProgram1 = [2,4,5,4,33,3]
-testProgram2 = [1002,4,3,4,33]
+data Opcode = ADD|MULT|INPUT|OUTPUT|HALT|INVALID deriving Show
+
+data ParamType = Positional|Immediate deriving Show
+data Param = Param Int ParamType deriving Show
+
+data Instruction =
+  Instruction { opcode :: Opcode
+              , param1 :: Param
+              , param2 :: Param
+              , param3 :: Param
+              } deriving Show
+
+testProgram1 = [2,4,5,5,99,3]  :: [Int]
+testProgram2 = [1002,4,3,4,33] :: [Int]
+testProgram3 = [1101,100,-1,4,0] :: [Int]
 
 main = do
   s <- readFile "day5-input.txt"
   putStrLn "Running Short Test Programs"
   
-  let testOut1 = runProgram testProgram1
+  let testOut1 = runProgram testProgram3
   putStrLn $ show testOut1
   putStrLn "hello"
 
@@ -46,36 +59,77 @@ updateCounter cs = cs {counter = (counter cs) + 4}
 
 executeAtPosition :: ComputerState -> ComputerState
 executeAtPosition cs =
-  let instruction = getIntcodeAt (counter cs) (program cs)
-      intcode = head instruction
+  let instruction = getNextInstruction cs
   in
-    case intcode of
-    1 ->
-      let newProgram = add (instruction !! 1) (instruction !! 2) (instruction !! 3) $ program cs
+    case opcode instruction of
+    ADD ->
+      let p1val = param2value (param1 instruction) (program cs)
+          p2val = param2value (param2 instruction) (program cs)
+          p3pos = getParamValue (param3 instruction)
+          newProgram = add p1val p2val p3pos $ program cs
       in
         ComputerState newProgram (counter cs) OK
-    2 ->
-      let newProgram = multiply (instruction !! 1) (instruction !! 2) (instruction !! 3) $ program cs
+
+    MULT ->
+      let p1val = param2value (param1 instruction) (program cs)
+          p2val = param2value (param2 instruction) (program cs)
+          p3pos = getParamValue (param3 instruction)
+          newProgram = multiply p1val p2val p3pos $ program cs
       in
         ComputerState newProgram (counter cs) OK
 
     _ -> ComputerState (program cs) (counter cs) BADINSTRUCTION
         
     
+getNextInstruction :: ComputerState -> Instruction
+getNextInstruction cs = 
+  let rawInstruction = drop (counter cs) (program cs)
+      firstValue = rawInstruction !! 0
+      rawOpcode  = firstValue `mod` 100
+      param1type = case (firstValue `mod` 1000) `div` 100 of
+        1 -> Immediate
+        0 -> Positional
+      param2type = case (firstValue `mod` 10000) `div` 1000 of
+        1 -> Immediate
+        0 -> Positional
+  in
+      
+  Instruction { opcode = parseOpcode rawOpcode
+              , param1 = Param (rawInstruction !! 1) Immediate
+              , param2 = Param (rawInstruction !! 2) Immediate
+              , param3 = Param (rawInstruction !! 3) Positional
+              }
 
+parseOpcode :: Int -> Opcode
+parseOpcode i = case i of
+  1 -> ADD
+  2 -> MULT
+  3 -> INPUT
+  4 -> OUTPUT
+  99 -> HALT
+  _ -> INVALID
+
+
+param2value :: Param -> Program -> Int
+param2value param program =
+  case param of
+    Param i Immediate -> i
+    Param i Positional -> program !! i
+
+getParamValue (Param i _) = i
+  
+  
 getIntcodeAt pos program =
   take 4 $ drop pos program
 
-add x_pos y_pos result_pos program =
-  let x_val = program !! x_pos
-      y_val = program !! y_pos
+add x_val y_val result_pos program =
+  let 
       sum = x_val + y_val
   in
     (take result_pos program) ++ [sum] ++ (drop (result_pos + 1) program)
 
-multiply x_pos y_pos result_pos program =
-  let x_val = program !! x_pos
-      y_val = program !! y_pos
+multiply x_val y_val result_pos program =
+  let 
       product = x_val * y_val
   in
     (take result_pos program) ++ [product] ++ (drop (result_pos + 1) program)
