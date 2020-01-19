4f62bc786dde93522d06cf649a226ea76e64fa95
Steve Anderson <steve@bootladder.com>
2020-01-18 19:32:20 -0500

    run N steps

--- day12.hs
+++ day12.hs
@@ -13,7 +13,7 @@ data Moon = Moon { xPos::Int
                  , xVel::Int
                  , yVel::Int
                  , zVel::Int
-                 } deriving Show
+                 } deriving (Show, Eq)
 
 parseMoon :: String -> Moon
 parseMoon str =
@@ -31,16 +31,59 @@ parseMoon str =
 testMoons :: [Moon]
 testMoons = map parseMoon $ lines testInput1
 
-testPairs :: [Moon] -> [(Moon,Moon)]
-testPairs z =
+getAllPairs :: [Moon] -> [(Moon,Moon)]
+getAllPairs z =
   if length z == 2
   then [(head z, last z)]
   else
-    [(head z, i) | i<-tail z ] ++ (testPairs $ tail z)
+    [(head z, i) | i<-tail z ] ++ (getAllPairs $ tail z)
 
+calculateVelocity :: Moon -> [Moon] -> Moon
+calculateVelocity moon moons =
+  let otherMoons = filter (\m -> m /= moon) moons
+      deltaVelX = sum $ map (\m -> compareField xPos m moon) otherMoons
+      deltaVelY = sum $ map (\m -> compareField yPos m moon) otherMoons
+      deltaVelZ = sum $ map (\m -> compareField zPos m moon) otherMoons
+  in
+    moon { xVel = (xVel moon) + deltaVelX
+         , yVel = (yVel moon) + deltaVelY
+         , zVel = (zVel moon) + deltaVelZ
+         }
+
+  where compareField f a b =
+                  if (f a) > (f b)
+                  then 1
+                  else if (f a) < (f b)
+                       then -1
+                       else 0
+
+calculatePosition :: Moon -> Moon
+calculatePosition m =
+  m { xPos = (xPos m) + (xVel m)
+    , yPos = (yPos m) + (yVel m)
+    , zPos = (zPos m) + (zVel m)
+    }
+
+
+simulationStep :: [Moon] -> [Moon]
+simulationStep moons =
+  let calcVels = map (\m -> calculateVelocity m moons) moons
+      calcPoss = map calculatePosition calcVels
+  in
+    calcPoss
+
+
+runSimulationNSteps :: Int -> [Moon] -> [Moon]
+runSimulationNSteps n moons =
+  let step = simulationStep moons
+  in
+    if n == 0 then moons else runSimulationNSteps (n-1) step
 
 main = do
   putStrLn testInput1
 
   putStrLn "hehlo"
-  putStrLn $ concat $ map ((++ "\n") . show ) $ testPairs testMoons
+  putStrLn $ concat $ map ((++ "\n") . show ) $ getAllPairs testMoons
+
+printList :: (Show a) => [a] -> IO ()
+printList = mapM_  (putStrLn . show)
