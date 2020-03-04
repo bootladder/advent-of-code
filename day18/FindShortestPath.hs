5cc88dc0f309994c0fdef5bcdf15ae16f93f1ed8
Steve Anderson <steve@bootladder.com>
2020-03-04 18:40:24 -0500

    helpers for FindShortestPath

--- day18/FindShortestPath.hs
+++ day18/FindShortestPath.hs
@@ -2,6 +2,43 @@ module FindShortestPath where
 
 import FindKeyDistances
 import Data.Char
+import Debug.Trace
+
+getKeyByName :: Char -> [Key] -> Key
+getKeyByName name keys =
+  head $ filter (\k -> keyName k == name) keys
+
+removeKeyByName :: Char -> [Key] -> [Key]
+removeKeyByName name keys =
+  let removeKeyFromKeys ks =
+        filter (\key -> keyName key /= name) ks
+
+      removeKeyFromOtherKeys otherKeys =
+        filter
+        (\(name'',_,_) -> name'' /= name)
+        otherKeys
+
+      removeKeyFromOtherKeysAllKeys ks =
+        map
+        (\key -> key {otherKeyDistancesAndDoors =
+                         removeKeyFromOtherKeys (otherKeyDistancesAndDoors key)
+                     })
+        ks
+
+  in
+    (removeKeyFromKeys $ removeKeyFromOtherKeysAllKeys keys)
+
+removeDoorNameFromKeys :: Char -> [Key] -> [Key]
+removeDoorNameFromKeys name keys =
+  let
+    removeDoorNameFromKey key =
+      key { otherKeyDistancesAndDoors =
+              map
+              (\(a,b,doors) -> (a,b,filter ((/=) name) doors))
+              (otherKeyDistancesAndDoors key)
+          }
+  in
+    map removeDoorNameFromKey keys
 
 findAllPossiblePaths :: [Key] -> [Char]
 findAllPossiblePaths keys =
@@ -18,31 +55,10 @@ findAllPossiblePathsStartingAt :: [Key] ->
 findAllPossiblePathsStartingAt keys startingKey doorsNamesOpened keyNamesFound distance =
   let
     keysWithStartingKeyRemoved =
-      map (\key ->
-             key { otherKeyDistancesAndDoors =
-                   filter (\(name,_,_) ->
-                             name /= (keyName startingKey))
-                   (otherKeyDistancesAndDoors key)
-                 }
-          )
-      keys
-
+      removeKeyByName (keyName startingKey) keys
 
     keysWithDoorRemoved =
-      map (\key ->
-             key { otherKeyDistancesAndDoors =
-                   map (\(a,b,doors) ->
-                          (a,b,
-                            (
-                              filter (\door ->
-                                        (keyName startingKey) /= door)
-                              doors
-                            ))
-                       )
-                       (otherKeyDistancesAndDoors key)
-                 }
-          )
-      keysWithStartingKeyRemoved
+      removeDoorNameFromKeys (toUpper $ keyName startingKey) keysWithStartingKeyRemoved
 
     possibleNextChoices = filter
                           (\(name, _, doors) ->
@@ -51,18 +67,22 @@ findAllPossiblePathsStartingAt keys startingKey doorsNamesOpened keyNamesFound d
                           otherKeyDistancesAndDoors startingKey
 
   in
+    trace ("\nCURRENT DISTANCE : " ++ show distance) $
+    trace ("\nSTARTING KEY : " ++ show startingKey) $
+    trace ("\nNUM CHOICES : " ++ show possibleNextChoices) $
     if length possibleNextChoices == 0
     then
       [keyName startingKey]
     else
-      concat $ map (\(name,d,doors) ->
-                       findAllPossiblePathsStartingAt
-                       keysWithDoorRemoved
-                       (head $ filter (\k -> keyName k == name) keysWithStartingKeyRemoved)
-                       (doorsNamesOpened ++ [toUpper name])
-                       (keyNamesFound ++ [name])
-                       (distance + d)
-                   )
+      concat $
+      map (\(name,d,doors) ->
+              findAllPossiblePathsStartingAt
+              keysWithDoorRemoved
+              (getKeyByName name keysWithStartingKeyRemoved)
+              (doorsNamesOpened ++ [toUpper name])
+              (keyNamesFound ++ [name])
+              (distance + d)
+          )
       possibleNextChoices
 
 
--- day18/FindShortestPathTest.hs
+++ day18/FindShortestPathTest.hs
@@ -9,12 +9,65 @@ main :: IO()
 main =
   do
     spec1
+    spec2
 
 spec1 =
   hspec $
   before
   (do
       grid <- readGridFromFile "day18-input-2.txt"
+      --putStrLn $ gridToString grid
+      return grid
+  ) $ do
+
+  describe "Remove Starting Key, Input 2 Data" $ do
+    it "Should Remove Key a" $ \grid -> do
+      let keys = getAllKeysDistancesAndDoors grid
+          keysWithOneRemoved = removeKeyByName 'a' keys
+
+      let matches = filter (\k -> keyName k == 'a') keysWithOneRemoved
+
+      length matches `shouldBe` 0
+
+    it "Should remove a from otherkeylist" $ \grid -> do
+      let keys = getAllKeysDistancesAndDoors grid
+          keysWithOneRemoved = removeKeyByName 'a' keys
+
+      let matches = filter
+                    (\(n,_,_) -> n == 'a')
+                    (otherKeyDistancesAndDoors (head keysWithOneRemoved))
+
+      length matches `shouldBe` 0
+
+
+  describe "Get Key By Name Input 2 Data" $ do
+    it "" $ \grid -> do
+      let keys = getAllKeysDistancesAndDoors grid
+          keyA = getKeyByName 'a' keys
+
+      keyName keyA `shouldBe` 'a'
+
+
+  describe "Remove Door from DoorList in Keys Input 2 Data" $ do
+    it "" $ \grid -> do
+      let keys = getAllKeysDistancesAndDoors grid
+          keysWithDoorRemoved = removeDoorNameFromKeys 'B' keys
+
+      let
+          keyC = getKeyByName 'c' keysWithDoorRemoved
+          matches = filter
+                    (\(_,_,doors) -> 'B' `elem` doors)
+                    (otherKeyDistancesAndDoors keyC)
+
+      --putStrLn $ show keyC
+      length matches `shouldBe` 0
+
+
+spec2 =
+  hspec $
+  before
+  (do
+      grid <- readGridFromFile "day18-input-1.txt"
       putStrLn $ gridToString grid
       return grid
   ) $ do
@@ -25,5 +78,5 @@ spec1 =
       let keys = getAllKeysDistancesAndDoors grid
       let allPossiblePaths = findAllPossiblePaths keys
 
+      --putStrLn $ show keys
       putStrLn $ show allPossiblePaths
-
