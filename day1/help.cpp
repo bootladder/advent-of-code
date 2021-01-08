3e75e165cc124c93b7fc950480ea5177f1fd7323
Steve Anderson <steve@bootladder.com>
2021-01-08 03:01:10 -0500

    day1 done

--- day1/help.cpp
+++ day1/help.cpp
@@ -14,3 +14,51 @@ void read_lines_ints(fstream& fstream, vector<int> & entries) {
     entries.insert(entries.end(),i);
   }
 }
+
+
+vector<int> find_two_values_with_sum_in_vector(vector<int> entries, int sum) {
+  vector<int> values;
+  int lowest_index = 0;
+  int highest_index = 0;
+  int lowest_value = 0;
+  int highest_value = 0;
+
+  highest_index = (int)entries.size() - 1;
+
+  bool found = false;
+
+  do {
+    lowest_value = entries.at(lowest_index);
+    highest_value = entries.at(highest_index);
+//    cout << "lowets L: " << lowest_value << endl;
+//    cout << "hiiest L: " << highest_value << endl;
+//    cout << "lowest Index: " << lowest_index << endl;
+//    cout << "highest Index: " << highest_index << endl;
+
+    if(lowest_value + highest_value == sum){
+      found = true;
+      break;
+    }
+
+    if(lowest_value + highest_value < sum){
+      lowest_index++;
+      continue;
+    }
+    if(lowest_value + highest_value > sum){
+      highest_index--;
+      continue;
+    }
+  }
+  while(lowest_index < entries.size()-1 && highest_index > 0);
+
+//  cout << "lowets L: " << lowest_value << endl;
+//  cout << "hiiest L: " << highest_value << endl;
+
+  if(found){
+    cout << "MATCH!!" << endl;
+    values.push_back(lowest_value);
+    values.push_back(highest_value);
+  }
+
+  return values;
+}
